d91ceac5d18d81bb387d67c8de789f7fd08ecc9a
Samuel Lijin <sxlijin@gmail.com>
2017-04-27 03:49:30 -0500

    diffcore-rename: remove record_if_better() which is now unused

--- diffcore-rename.c
+++ diffcore-rename.c
@@ -356,22 +356,6 @@ static int find_exact_renames(struct diff_options *options)
 	return renames;
 }
 
-#define NUM_CANDIDATE_PER_DST 4
-static void record_if_better(struct diff_score m[], struct diff_score *o)
-{
-	int i, worst;
-
-	/* find the worst one */
-	worst = 0;
-	for (i = 1; i < NUM_CANDIDATE_PER_DST; i++)
-		if (score_compare(&m[i], &m[worst]) > 0)
-			worst = i;
-
-	/* is it better than the worst one? */
-	if (score_compare(&m[worst], o) > 0)
-		m[worst] = *o;
-}
-
 /*
  * Returns:
  * 0 if we are under the limit;
